66f3d76e08fa70246d7936bc8685d2e412d845f7
kdkibtops <mustafa.heidar.mh@gmail.com>
2024-03-20 23:47:43 +0200

    First steps of prisma

--- prisma/models/patients.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-export const testPrismDB = async () => {
-	const connected = await prisma.$connect();
-	console.log(connected);
-};
